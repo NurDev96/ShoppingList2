0674dfad97f0c32ea721d3d11708c02000436b25
Nurjanbek A <nurjanbek2196@gmail.com>
2023-03-29 09:45:53 +0500

    Presentation layout, ViewModel

---
+++ app/src/main/java/com/example/shoppinglist_2/presentation/MainViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.shoppinglist_2.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shoppinglist_2.data.ShopListRepositoryImpl
+import com.example.shoppinglist_2.domain.*
+
+class MainViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList() {
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun changeEnabledState(shopItem: ShopItem) {
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
+    }
+}
